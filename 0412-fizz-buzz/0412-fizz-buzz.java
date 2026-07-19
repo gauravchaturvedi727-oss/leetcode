5876986c608beabe3520219be1ea6acdb830d526
Gourav Chaturvedi <gauravchaturvedi727@gmail.com>
2026-07-19 22:55:50 +0530

    Time: 1 ms (99.86%), Space: 46.8 MB (46.01%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -5,7 +5,7 @@ class Solution {
 
         for(int i = 1; i <= n; i++){
 
-            if(i % 15 == 0){
+            if(i % 3 == 0 && i % 5 == 0){
                 list.add("FizzBuzz");
             }
             else if(i % 3 == 0){
